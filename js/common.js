fc582fb0279c8e1df4a5ac7fd3fa9ecfa13b55e9
Alvin <goape7914@gmail.com>
2021-09-30 17:11:46 +0800

    9/30 1.修改放大鏡閃光在懸賞單出來時消失 2.修改遊戲場景文字 3.修改遊戲場景最後發光鯛和文字出現的時間 4.刪除開始遊戲按鈕 5.新增靜音/播放按鈕

--- js/common.js
+++ js/common.js
@@ -1,18 +1,25 @@
 let timer = true;
 let bgm = "";
+let muted = true;
+// 0930修改播放音樂邏輯
 function playAudio(dataName) {
     let audio = $(`audio[data-name="${dataName}"]`);
-    // let ct = $(audio).attr("currentTime")
- 
-    $(audio).trigger("play");
-    $(audio).prop("currentTime",0)
+    // 如果靜音，不撥放
+    if (muted) {
+        return;
+        // 非靜音狀態，且撥放的是BGM，撥放並且不重製播放時間
+    } else if (!muted && dataName === bgm) {
+        var playPromise = $(audio).trigger("play");
+        // 其餘音效，每次皆重頭撥放
+    } else {
+        $(audio).prop("currentTime", 0);
+        $(audio).trigger("play");
+    }
 }
 function pauseAudio(dataName) {
     let audio = $(`audio[data-name="${dataName}"]`);
     $(audio).trigger("pause");
-    $(audio).prop("currentTime",0)
 }
-
 function dNone(className) {
     $(`${className}`).addClass("d-none");
 }
@@ -31,17 +38,23 @@ function transScene500ms(preScene, nextScene) {
     }, 1000);
 }
 
-// loading > intro
+// 初始化設定
 $(function () {
+    // 設定網頁高度為瀏覽器"內"高
+    let winInnerHeight = $(window).innerHeight();
+    $(".wrapper").css({ height: winInnerHeight });
+
     // 阻止雙擊放大
-    $(document).on("dbclick",function(e){
+    $(document).on("dbclick", function (e) {
         e.preventDefault();
-    })
-    $(document).on("gesturestart",function(e){
+    });
+    // 阻止多點觸控
+    $(document).on("gesturestart", function (e) {
         e.preventDefault();
-    })
+    });
+    // 前後點擊時間小於300ms，阻止事件
     let lastTouchEnd = 0;
-    $(document).on("touchend",function(e) {
+    $(document).on("touchend", function (e) {
         let now = new Date().getTime();
         if (now - lastTouchEnd <= 300) {
             e.preventDefault();
@@ -49,43 +62,65 @@ $(function () {
         lastTouchEnd = now;
     });
 
-    // 網頁縮小停止音樂
-    $(document).on("visibilitychange",(e)=> {
-        // 網頁可見
-        if(document.visibilityState==='visible'){
-            console.log("vis")
-            playAudio(bgm)
-        }else{
-            // 不可見
-            console.log("hide")
-            pauseAudio(bgm)
+    // 監聽網頁是否可處於可見狀態
+    $(document).on("visibilitychange", (e) => {
+        // 網頁可見撥放BGM
+        if (document.visibilityState === "visible") {
+            playAudio(bgm);
+        } else {
+            // 不可見停止BGM
+            pauseAudio(bgm);
         }
-    })
-   
-      
-    // 初始化設定網頁高度為瀏覽器"內"高
-    let winInnerHeight = $(window).innerHeight();
-    $(".wrapper").css({ height: winInnerHeight });
-
-    // 按下按鈕開始遊戲
-    $(".start_game_btn").on("click", function () {
-        $(this).addClass("d-none");
-        playAudio("click");
-        dShow(".container");
-        bgm = "BGM-suspense";
-        playAudio("BGM-suspense");
-        pauseAudio("BGM-suspense")
-        // 4s 音樂開始
-        setTimeout(() => {
-            dShow(".intro_scene");
-            playAudio("BGM-suspense");
-        }, 6000);
-
-        // 9.5s loading_scene消失
-        setTimeout(() => {
-            dNone(".loading_scene");
-        }, 11500);
     });
+
+    // 音樂開關鈕
+    $(".audio_btn").on("click", function () {
+        if (muted) {
+            muted = !muted;
+            playAudio(bgm);
+            $(".audio_btn img").toggleClass("d-none");
+        } else {
+            muted = !muted;
+            pauseAudio(bgm);
+            $(".audio_btn img").toggleClass("d-none");
+        }
+    });
+});
+
+// loading > intro
+$(function () {
+    bgm = "BGM-suspense";
+    setTimeout(() => {
+        dShow(".intro_scene");
+        dShow(".audio_btn");
+        playAudio("BGM-suspense");
+    }, 6000);
+
+    // 9.5s loading_scene消失
+    setTimeout(() => {
+        dNone(".loading_scene");
+    }, 11500);
+
+    // 0930刪除開始按鈕
+    // 按下按鈕開始遊戲
+    // $(".start_game_btn").on("click", function () {
+    //     $(this).addClass("d-none");
+    //     playAudio("click");
+    //     dShow(".container");
+    //     bgm = "BGM-suspense";
+    //     playAudio("BGM-suspense");
+    //     pauseAudio("BGM-suspense")
+    //     // 4s 音樂開始
+    //     setTimeout(() => {
+    //         dShow(".intro_scene");
+    //         playAudio("BGM-suspense");
+    //     }, 6000);
+
+    //     // 9.5s loading_scene消失
+    //     setTimeout(() => {
+    //         dNone(".loading_scene");
+    //     }, 11500);
+    // });
 });
 
 // intro_scene
@@ -93,6 +128,7 @@ $(function () {
     // 第一個對話框 > 第二對話框
     $(".intro_scene").one("click", function () {
         dNone(".chat_bubble.first");
+        $(".spark_box").css("opacity", "0");
         dShow(".chat_bubble.second.d-none");
         dShow(".post_rising");
         setTimeout(() => {
@@ -110,12 +146,15 @@ $(function () {
 
     // 按下接受任務 過場進game_scene
     $(".accept_mission").on("click", function () {
-        if (timer === true){
+        if (timer === true) {
             timer = false;
             playAudio("click");
             bgm = "BGM-speed";
             playAudio("BGM-speed");
-            pauseAudio("BGM-speed")
+            // setTimeout(() => {
+            //     pauseAudio("BGM-speed");
+            // }, 150);
+            pauseAudio("BGM-speed");
             // 印章出現
             dShow(".letsgo");
             //1s 後出現過場黑幕
@@ -135,7 +174,7 @@ $(function () {
                 dShow(".game_scene");
                 playAudio("BGM-speed");
             }, 2500);
-            
+
             //5s 過場黑幕 display none
             setTimeout(() => {
                 dNone(".trans_scene");
@@ -143,7 +182,6 @@ $(function () {
 
             // 進入遊戲後五秒才能點擊鯛
             setTimeout(() => {
-                console.log("pointer-events:auto");
                 $(".diu").css({ "pointer-events": "auto" });
                 timer = true;
             }, 13000);
@@ -223,7 +261,9 @@ $(function () {
                 $(".shrink").addClass("active");
                 $(".fish").addClass("active");
                 dNone(".chat_bubble");
-                dShow(".game_third");
+                setTimeout(() => {
+                    dShow(".game_third");
+                }, 4000);
             }
         }
         // 發光的鯛
@@ -233,13 +273,13 @@ $(function () {
                 $(".float_up5").addClass("active");
                 $(".game_scene").addClass("active");
                 playAudio("prize");
-                pauseAudio("prize");             
+                pauseAudio("prize");
                 setTimeout(function () {
                     $(".game_scene").hide();
                     dShow(".prize_scene");
                     playAudio("prize");
                     timer = true;
-                }, 4000);    
+                }, 4000);
                 // 按鈕可點擊時間
                 setTimeout(function () {
                     $(".prize_scene .btn_box").css("pointer-events", "auto");
@@ -251,7 +291,6 @@ $(function () {
 
 // prize_scene
 $(function () {
-
     // 點擊換場
     $(".prize_scene .btn_box").on("click", function (e) {
         // dShow(".trans_scene.s_500");
@@ -263,9 +302,9 @@ $(function () {
         //     dNone(".trans_scene");
         // }, 1000);
         transScene500ms(".prize_scene", ".qrcode_scene");
-        pauseAudio("BGM-speed")
-        bgm = "BGM-happy"
-        playAudio("BGM-happy")
+        pauseAudio("BGM-speed");
+        bgm = "BGM-happy";
+        playAudio("BGM-happy");
         setTimeout(function () {
             $(".qrcode_scene .btn_box").css("pointer-events", "auto");
         }, 3000);
@@ -274,19 +313,18 @@ $(function () {
 
 // qrcode_scene
 $(function () {
-
     $(".qrcode_scene .btn_box").on("click", function () {
         playAudio("click");
         transScene500ms(".qrcode_scene", ".sushi_scene");
-      
-        if (timer===true) {
+
+        if (timer === true) {
             timer = false;
             playAudio("prize");
-            pauseAudio("prize")
-            setTimeout(function(){
+            pauseAudio("prize");
+            setTimeout(function () {
                 playAudio("prize");
-                timer=true;
-            },1000);
+                timer = true;
+            }, 1000);
             setTimeout(function () {
                 $(".sushi_scene .btn_box").css("pointer-events", "auto");
             }, 3000);
@@ -295,7 +333,7 @@ $(function () {
 });
 
 // sushi_scene
-$(".sushi_scene .btn_box").on("click",function () {
+$(".sushi_scene .btn_box").on("click", function () {
     dNone(".sushi_scene");
-    dShow(".lottery")
-})
+    dShow(".lottery");
+});
